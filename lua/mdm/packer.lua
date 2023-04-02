01dee2ba9b5dcddfedfb3fe9d1d70fe4fde7a2ee
Marc Dominik Migge <marcmigge@gmx.net>
2023-04-02 12:17:59 +0200

    Add bufferline package

--- lua/mdm/packer.lua
+++ lua/mdm/packer.lua
@@ -14,6 +14,11 @@ return require('packer').startup(function(use)
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use {
+      'akinsho/bufferline.nvim',
+      tag = "v3.*",
+      requires = 'nvim-tree/nvim-web-devicons'
+    }
 
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.0' }
     use 'nvim-telescope/telescope-ui-select.nvim'
